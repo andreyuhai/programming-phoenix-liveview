74bd29f69aae0d697e792c90f452777a6e281277
Burak <burak@llinformatics.com>
2021-07-13 12:56:12 +0200

    Add seeds for product

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description: "The classic strategy game",
+    sku: 5_678_910,
+    unit_price: 10.00
+  },
+  %{
+    name: "Tic-Tac-Toe",
+    description: "The game of Xs and Os",
+    sku: 11_121_314,
+    unit_price: 3.00
+  },
+  %{
+    name: "Table Tennis",
+    description: "Bat the ball back and forth. Don't miss!",
+    sku: 15_222_314,
+    unit_price: 12.00
+  },
+]
+
+Enum.each(products, fn product -> Catalog.create_product(product) end)
