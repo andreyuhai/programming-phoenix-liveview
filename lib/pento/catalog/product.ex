c9fa88dfbbced9e5ff02a54f451d50696c6ba5f6
Burak <burak@llinformatics.com>
2021-07-13 12:56:49 +0200

    Create another changeset in the Product schema that only changes the unit_price field and only allows for a price decrease from the current price

--- lib/pento/catalog/product.ex
+++ lib/pento/catalog/product.ex
@@ -17,5 +17,12 @@ defmodule Pento.Catalog.Product do
     |> cast(attrs, [:name, :description, :unit_price, :sku])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
+    |> validate_number(:unit_price, greater_than: 0.0)
+  end
+
+  def unit_price_changeset(product, attrs) do
+    product
+    |> cast(attrs, [:unit_price])
+    |> validate_number(:unit_price, less_than: product.unit_price)
   end
 end
